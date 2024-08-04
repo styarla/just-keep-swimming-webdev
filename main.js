639faff2acee695c8d638348b40132013baab950
styarla <styarla@gmail.com>
2024-08-04 23:15:31 +0530

    added suprsend notification bar

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { initSuprSendInbox } from "@suprsend/web-inbox";
 
 const scene = new THREE.Scene();
 
@@ -79,9 +80,129 @@ function animate() {
   renderer.render( scene, camera );
 }
 
-animate()
+animate();
 
+initSuprSendInbox(document.getElementById("suprsend-inbox"), suprSendConfig);
+const targetElement = docuent.getElementById("suprsend-inbox");
+cleanSuprSend(targetElement);
 
+const suprSendConfig={
+  workspaceKey: "your_workspace_key",
+  distinctId: "your_distinct_id",
+  subscriberId: "your_subscriber_id",
+  tenantId: "testing",
+  themeType: "dark",
+  hideToast:true,
+  theme: {
+    bell: { color: "blue" },
+    badge: { backgroundColor: "red", color: "black" },
+  }
+}
 
+// full theme example
 
+const colors = {
+  primary: '#2E70E8',
+  primaryText: '#EFEFEF',
+  secondaryText: '#CBD5E1',
+  border: '#3A4A61',
+  main: '#1D2635',
+  error: '#F97066'
+}
 
+const themeExample= {
+  bell: { color: '#fff' },
+  badge: { backgroundColor: darkColors.primary },
+  header: {
+    container: {
+      backgroundColor: darkColors.main,
+      borderBottom: `0.5px solid ${darkColors.border}`,
+      boxShadow: '0 0 5px 0 rgba(0, 0, 0, 0.5)'
+    },
+    headerText: { color: darkColors.primaryText },
+    markAllReadText: { color: darkColors.primary }
+  },
+  tabs: {
+    color: darkColors.primaryText,
+    unselectedColor: darkColors.secondaryText + 'D9',
+    bottomColor: darkColors.primary,
+    badgeColor: 'rgba(100, 116, 139, 0.5)',
+    badgeText: darkColors.primaryText
+  },
+  notificationsContainer: {
+    container: {
+      backgroundColor: darkColors.main,
+      borderColor: darkColors.border
+    },
+    noNotificationsText: {
+      color: darkColors.primaryText
+    },
+    noNotificationsSubtext: {
+      color: darkColors.secondaryText
+    },
+    loader: { color: darkColors.primary }
+  },
+  notification: {
+    container: {
+      borderBottom: `1px solid ${darkColors.border}`,
+      readBackgroundColor: darkColors.main,
+      unreadBackgroundColor: '#273244',
+      hoverBackgroundColor: '#2D3A4D'
+    },
+    pinnedText: {
+      color: darkColors?.secondaryText
+    },
+    pinnedIcon: {
+      color: 'red'
+    },
+    headerText: { color: darkColors.primaryText },
+    bodyText: {
+      color: darkColors.secondaryText,
+      blockquoteColor: 'rgba(100, 116, 139, 0.5)'
+    },
+    unseenDot: { backgroundColor: darkColors.primary },
+    createdOnText: { color: darkColors.secondaryText },
+    subtext: { color: '#94a3b8' },
+    actions: [
+      { container: { backgroundColor: darkColors.primary } },
+      {
+        container: {
+          borderColor: darkColors.border,
+          backgroundColor: 'transparent',
+          hoverBackgroundColor: darkColors.main
+        },
+        text: { color: darkColors.secondaryText }
+      }
+    ],
+    expiresText: {
+      backgroundColor: 'rgba(100, 116, 139, 0.5)',
+      color: darkColors.secondaryText,
+      expiringBackgroundColor: 'rgba(217, 45, 32, 0.15)',
+      expiringColor: darkColors.error
+    },
+    actionsMenuIcon: {
+      color: darkColors.secondaryText,
+      hoverBackgroundColor: 'rgba(100, 116, 139, 0.5)'
+    },
+    actionsMenu: {
+      backgroundColor: darkColors.main,
+      borderColor: darkColors.border
+    },
+    actionsMenuItem: { hoverBackgroundColor: 'rgba(100, 116, 139, 0.2)' },
+    actionsMenuItemIcon: { color: darkColors.secondaryText },
+    actionsMenuItemText: {
+      color: darkColors.secondaryText
+    }
+  },
+  toast: {
+    container: {
+      backgroundColor: darkColors.main,
+      borderColor: darkColors.border
+    },
+    headerText: { color: darkColors.primaryText },
+    bodyText: {
+      color: darkColors.secondaryText,
+      blockquoteColor: darkColors.border
+    }
+  }
+}
